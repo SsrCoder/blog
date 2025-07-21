c7f29cfb476e10573a176fdcbdb0d08ef742582a
SsrCoder <SsrCoder@gmail.com>
2025-07-21 22:08:00 +0800

    feat: init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,4 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-import partytown from '@astrojs/partytown';
-
-import tailwind from '@astrojs/tailwind';
-
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://blog-next.ssrcoder.com',
-
-  // trailingSlash: 'always',
-  prefetch: true,
-
-  markdown: {
-    shikiConfig: {
-      themes: {
-        light: 'github-light',
-        dark: 'github-dark',
-      },
-    },
-  },
-
-  integrations: [partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  }), tailwind()],
-});
+export default defineConfig({});
