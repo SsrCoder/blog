a781b94a27859181249feeaf6d8c47ba4383554b
SsrCoder <SsrCoder@gmail.com>
2025-07-29 19:48:21 +0800

    feat: code fullscreen

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,8 +1,10 @@
 import { defineEcConfig } from 'astro-expressive-code'
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
+import { pluginFullscreen } from 'expressive-code-fullscreen'
 
 export default defineEcConfig({
 	plugins: [
 		pluginLineNumbers(), // https://expressive-code.com/plugins/line-numbers/
+		pluginFullscreen(), // https://frostybee.github.io/expressive-code-fullscreen/configuration/
 	],
 })
